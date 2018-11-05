f79e127bad7f855f4a0985c15c014ccdb3ea38be
paulcysu <su.paul.cy@gmail.com>
2018-11-05 14:13:07 -0800

    main page css changes

--- js/main.js
+++ js/main.js
@@ -465,7 +465,7 @@ $(document).on('ready', function () {
 	});
 
 	var i = 0;
-	var txt = '<Hi, I\'m a frontend developer!>'; /* The text */
+	var txt = 'Hi, I\'m a frontend developer.'; /* The text */
 	var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 	function typeWriter() {
